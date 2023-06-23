20c29c8f1c718f8cd3741f731261ac8b807f80a8
shaperhub <leonnwoke@gmail.com>
2023-06-22 20:53:25 -0500

    First push, List and Map working, no weather yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-function App() {
+import { getPlacesData } from './api';
+import { getWeatherData } from './api';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+    })
+  }, []);
+
+  useEffect(() => {
+    if (bounds) {
+        getPlacesData(bounds.sw, bounds.ne)
+        .then((data) => {
+            // console.log(data);
+
+            setPlaces(data);
+        })
+
+        getWeatherData(coordinates.lat, coordinates.lng)
+        .then((data) => {
+            console.log(data);
+            setWeatherData(data);
+        });
+    }
+    
+  }, [coordinates, bounds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <CssBaseline />
+        <Header />
+        <Grid container spacing={3} style={{width: '100%'}}>
+            <Grid item xs={12} md={4}>
+                <List 
+                    places={places}
+                />
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Map
+                    setCoordinates={setCoordinates}
+                    setBounds={setBounds}
+                    coordinates={coordinates}
+                    places={places}
+                />
+            </Grid>
+        </Grid>
+    </>
   );
 }
 
---
+++ src/api/index.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        'X-RapidAPI-Key': '235f5c3932msh6b871250f145c28p158405jsnae02c489bedc',
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    
+      const { data: { data } } = await axios.get('https://weatherapi-com.p.rapidapi.com/current.json', {
+        params: {q: `${lat},${lng}` },
+        headers: {
+          'X-RapidAPI-Key': '235f5c3932msh6b871250f145c28p158405jsnae02c489bedc',
+          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        },
+      });
+      // console.log("Weather Data: " + data);
+      return data;
+    
+  } catch (error) {
+    console.log("Weather Error: " + error);
+  }
+};
