36fde61a444ec7d864957edc5307a690d891655c
shaperhub <leonnwoke@gmail.com>
2023-06-23 18:23:53 -0500

    Autocomplete work after add API key in indexhtml

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ const App = () => {
   return (
     <>
         <CssBaseline />
-        <Header/>
+        <Header setCoords={setCoords}/>
         <Grid container spacing={3} style={{width: '100%'}}>
             <Grid item xs={12} md={4}>
                 <List 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles';
 
-const Header = ({ onPlaceChanged, onLoad }) => {
+const Header = ({ setCoords }) => {
 
   const classes = useStyles();
+  const [autocomplete, setAutocomplete] = useState(null);
+
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoords({ lat, lng });
+  };
 
   return (
     <AppBar position="static">
@@ -19,14 +30,14 @@ const Header = ({ onPlaceChanged, onLoad }) => {
                 <Typography variant='h6' className={classes.title}>
                     Explore New Places
                 </Typography>
-                {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}> */}
+                <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
                     <div className={classes.search}>
                         <div className={classes.searchIcon}>
                             <SearchIcon />
                         </div>
                         <InputBase placeholder='Search...' classes={{ root: classes.inputRoot, input: classes.inputInput }} />
                     </div>
-                {/* </Autocomplete> */}
+                </Autocomplete>
             </Box>
         </Toolbar>
     </AppBar>
